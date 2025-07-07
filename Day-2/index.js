a5b262ed23259b2f3ad230c38f1ace5f07614152
vaishnavi123467 <vaishnavigavhane03@gmail.com>
2025-07-07 23:13:59 +0530

    update for Day-2

---
+++ Day-2/index.js
@@ -0,0 +1,31 @@
+//*operators :--
+
+//  (==) check data not datatype :
+console.log(1 == 1);         // here the data (numbers) is same .   output - true
+console.log(1 == 2);         // here the data is diffrenet .        output - false      
+console.log(10 == 2);        // here the data is diffrenet .        output - false  
+console.log("10" == 2);      // here the data is diffrenet .        output - false  
+console.log("2" == 2);       // here the data (numbers) is same .   output - true
+
+
+// (===) check data and datatype :
+
+console.log("10" === 2);        // here the data if different and datatype is ("10"=string-datatype) and (2 = number-datatype)
+console.log("2" === 2);        // here the data (2===2) is same but the datatype is different ie ("2" is string-datatype)(2 = number-datatype).
+console.log("2" === "2");      // here both the data and datatype is same ie both number is 2 and datatype is string.
+
+
+
+console.log(2 > 1);
+console.log(2 > 10);
+console.log(2 < 1);
+console.log(2 < 10);
+
+console.log(2 <= 2);
+console.log(20 >= 20);
+console.log(20 >= 21);
+console.log(20 <= 21);
+
+
+
+
