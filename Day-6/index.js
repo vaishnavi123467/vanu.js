a2cdca5e6b1b9757fb39582fc39b33fae75d922d
vaishnavi123467 <vaishnavigavhane03@gmail.com>
2025-07-19 13:18:01 +0530

    update for Day-6

--- Day-6/index.js
+++ Day-6/index.js
@@ -43,6 +43,9 @@ for (let i = usersAge.length - 1; i >= 0; i--) {
 
 
 
+// map :
+// its return new array and to change each item in the array
+// return a new array
 
 // Q. no reduce only modification 
 let number = [1, 2, 3];
@@ -56,3 +59,26 @@ console.log(newArray,"newArray") //[5, 10, 15 ]
 let students = ["virat", "rohit", "rahul"];
 let updatedStudentsName = students.map((student) =>  student + " kohli" );
 console.log(updatedStudentsName, "updatedStudentsName");
+
+
+
+
+// foreach - to run function on each element and doesn't return new array.
+
+function greetWithName(name) {
+  console.log("Welcome! ", name);
+}
+let studentss = ["virat", "rohit", "rahul"];
+studentss.forEach((student) => greetWithName(student));
+
+
+
+
+
+// fitler - its return new array of elements , if element pass the condition .
+// It is used to reduce the element ex:[2,4,7,8,9] if we want only even number then we will get array as [2,4,8].
+
+let numb = [2,3,4,5,6];
+let evenNumbers = numb.filter( num => num % 2 == 0)
+console.log(evenNumbers)
+
